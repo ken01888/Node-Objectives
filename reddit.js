69595f724f8ecf2be039c2f47b09896a5084e4d1
ken01888 <athena201017@gmail.com>
2021-03-05 09:39:18 -0600

    scrapper

--- reddit.js
+++ reddit.js
@@ -5,22 +5,40 @@ const rp = require("request-promise");
 
 const redData = path.join(__dirname, "./popular-article.json");
 
-let obj;
+let arr=[]
+let obj = {}
 
-(async () => {
-  try {
-    res = await rp("https://reddit.com/r/popular.json");
-    body = await JSON.parse(res).data.children;
-    body.forEach((element, index) => {
-      let arr = [];
-      obj = {
-        id: index,
-        title: element.data.title,
-        author: element.data.author,
-        url: element.data.url,
-      };
-      arr.push(obj);
-      fs.appendFileSync(redData, JSON.stringify(arr) + "\n");
-    });
-  } catch (error) {}
-})();
+rp("https://reddit.com/r/popular.json").then((body) => {
+  JSON.parse(body).data.children.map((element) => {
+    ;
+     obj = {
+      title: element.data.title,
+      author: element.data.author,
+      url: element.data.url,
+    };
+
+    arr.push(obj);
+});
+fs.appendFileSync(redData, JSON.stringify(arr));
+});
+
+// (async () => {
+//   try {
+//     res = await rp("https://reddit.com/r/popular.json");
+//     body = await JSON.parse(res).data.children;
+//     let arr = [];
+
+//     body.forEach((element, index) => {
+//       obj = {
+//         id: index,
+//         title: element.data.title,
+//         author: element.data.author,
+//         url: element.data.url,
+//       };
+//       arr.push(obj);
+//       fs.appendFileSync(redData, JSON.stringify(arr) + "\n");
+//     });
+//   } catch (error) {
+//       console.log(error)
+//   }
+// })();
